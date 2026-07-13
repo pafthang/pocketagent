9a39c40ac00ce1db8d6b136e14ee6f65695b23aa
Pavel Rusakov <149431538+pafthang@users.noreply.github.com>
2026-07-14 01:14:55 +0300

    Improve config with .env support

--- internal/common/config.go
+++ internal/common/config.go
@@ -1,22 +1,27 @@
 package common
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 // Config holds common configuration
-
 type Config struct {
 	ServiceName string
 	Port        string
 	NatsURL     string
 	LogLevel    string
+	OllamaURL   string
 }
 
+// LoadConfig loads configuration from .env + env vars
 func LoadConfig() *Config {
+	_ = godotenv.Load() // load .env if exists
+
 	viper.SetDefault("port", "8080")
 	viper.SetDefault("nats_url", "nats://nats:4222")
 	viper.SetDefault("log_level", "info")
+	viper.SetDefault("ollama_url", "http://ollama:11434")
 
 	viper.AutomaticEnv()
 
@@ -25,5 +30,6 @@ func LoadConfig() *Config {
 		Port:        viper.GetString("PORT"),
 		NatsURL:     viper.GetString("NATS_URL"),
 		LogLevel:    viper.GetString("LOG_LEVEL"),
+		OllamaURL:   viper.GetString("OLLAMA_URL"),
 	}
 }
