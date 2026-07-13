e30aedba85d2b6e6cd47dec9f4e3f19ff730cf7c
Pavel Rusakov <149431538+pafthang@users.noreply.github.com>
2026-07-14 01:18:16 +0300

    Apply logging to nats-server

--- services/nats-server/main.go
+++ services/nats-server/main.go
@@ -8,28 +8,31 @@ import (
 	"syscall"
 
 	"github.com/nats-io/nats-server/v2/server"
+	"github.com/pafthang/pocketagent/internal/common"
 )
 
 func main() {
 	var port = flag.Int("port", 4222, "NATS port")
 	flag.Parse()
 
+	logger := common.NewSlogLogger("nats-server")
+
 	s := server.New(&server.Options{
-		Port: *port,
+		Port:      *port,
 		JetStream: true,
-		StoreDir: "/data",
+		StoreDir:  "/data",
 	})
 
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Embedded NATS with JetStream started on port %d", *port)
+	logger.Info("Embedded NATS with JetStream started", "port", *port)
 
-	// Graceful shutdown
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-	<-c
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
 
 	s.Shutdown()
+	logger.Info("NATS server shutdown complete")
 }
